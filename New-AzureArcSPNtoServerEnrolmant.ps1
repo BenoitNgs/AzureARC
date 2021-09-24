5abbb0a1c8faa4d68a3975a7e4d8ac047baa9be5
BenoitNgs <benoit.nugues@gmail.com>
2021-09-24 22:25:56 +0200

    Update New-AzureArcSPNtoServerEnrolmant.ps1

--- New-AzureArcSPNtoServerEnrolmant.ps1
+++ New-AzureArcSPNtoServerEnrolmant.ps1
@@ -16,5 +16,5 @@ Set-AzContext -Subscription $strSubID
 $objSPN = New-AzADServicePrincipal @param
 $credential = New-Object pscredential -ArgumentList "temp", $objSPN.Secret
 
-Write-Warning "AppID: $objSPN.ApplicationId"
+Write-Warning "AppID: $($objSPN.ApplicationId)"
 Write-Warning "AppSecret: $($credential.GetNetworkCredential().password)"
